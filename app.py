710ba6992da303307e3c288159f69e4d2ead1611
Raffi85 <50589200+Raffi85@users.noreply.github.com>
2025-05-19 12:24:00 +0800

    Debugging

--- app.py
+++ app.py
@@ -1769,5 +1769,5 @@ init_db()
 
 
 if __name__ == '__main__':
-    app.run(debug=False, host='0.0.0.0', port=5000)
+    app.run(debug=False, host='0.0.0.0', port=int(os.environ.get("PORT", 5000)))
 
